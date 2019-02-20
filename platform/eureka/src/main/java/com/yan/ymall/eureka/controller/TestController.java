71eb1f79b6f7031eb9a2576b95269dfa514331c0
yandh <445288363@qq.com>
2019-02-20 19:08:39 +0800

    提交代码

---
+++ platform/eureka/src/main/java/com/yan/ymall/eureka/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.yan.ymall.eureka.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RefreshScope
+public class TestController {
+
+
+}
---
+++ platform/gateway/src/main/java/com/yan/ymall/gateway/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.yan.ymall.gateway.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class TestController {
+
+    @Value("${testa}")
+    private String test;
+
+    @RequestMapping("/test")
+    public String test() {
+        return test;
+    }
+}
